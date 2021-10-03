109d3b2f756c5f997521e57a4872ce0864152585
Hiroya Hinomori <nexearth@gmail.com>
2021-10-03 21:10:23 +0900

    Add: shell scripts

---
+++ scripts/setup.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+set -eu
+
+export LANG=C
+
+if [ ! $(which mint) ]; then
+  echo "  + Installing Mint..."
+  brew install mint
+else
+  echo "  + Mint found."
+fi
+
+# if [ ! $(which sourcery) ]; then
+#   echo "  + Installing Sourcery..."
+#   brew install sourcery
+# else
+#   echo "  + Sourcery found."
+# fi
+
+# if [ ! $(which swiftlint) ]; then
+#   echo "  + Installing SwiftLint..."
+#   brew install swiftlint
+# else
+#   echo "  + SwiftLint found."
+# fi
+
+# if [ ! $(which swiftformat) ]; then
+#   echo "  + Installing SwiftFormat..."
+#   brew install swiftformat
+# else
+#   echo "  + SwiftFormat found."
+# fi
+
+# Bundler
+if test ! $(which bundle); then
+  echo "  + Installing bundler..."
+  gem install bundler
+else
+  echo "  + Bundler found."
+fi
+
+# # Gem
+# echo "  + Installing gems."
+# bundle install --path=vendor/bundle
+
+# cp .Rambafile Rambafile
+# bundle exec generamba template install
+# rm -rf Rambafile
