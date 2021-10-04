57fac250c70ab69556310d58f8ec3adad5922f05
Hiroya Hinomori <nexearth@gmail.com>
2021-10-05 00:45:01 +0900

    Add: xcodegen cache setting

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -5,7 +5,7 @@ set -eu
 # bootstrap
 
 echo "  + Generate xcodeproje by XcodeGen."
-mint run xcodegen
+mint run xcodegen generate --use-cache --cache-path .xcodegen/cache
 bundle exec pod install
 
 echo ""
