102f16530ba5734c5f848da7619f5992fc27b615
Hiroya Hinomori <nexearth@gmail.com>
2021-10-04 23:59:03 +0900

    Add: create project

---
+++ App/App.swift
@@ -0,0 +1,17 @@
+//
+//  App.swift
+//  App
+//
+//  Created by hiroya-hinomori on 2021/10/04.
+//
+
+import SwiftUI
+
+@main
+struct Main: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  App
+//
+//  Created by hiroya-hinomori on 2021/10/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
